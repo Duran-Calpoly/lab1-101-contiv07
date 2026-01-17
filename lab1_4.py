b6bc67fa7d61a3cde03abd7a0bf06af2db1b0acf
contiv07 <ontiveroscesar58@gmail.com>
2026-01-17 02:39:58 +0000

    Completed Lab 1

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,11 @@
-pass
+def calculate_average(num1, num2, num3):
+    "calculates the average of three numbers"
+    return (num1 + num2 + num3) / 3
+
+def add_tax(bill_total):
+    "Returns the total after adding 10% tax"
+    return bill_total *1.10
+
+def greet_user(name):
+    "greets user after inputing name"
+    return "Hello " + name
